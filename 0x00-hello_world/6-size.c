f556f7c15b6356584bc99067998f2a13528293da
halimamouchrif <mouchrifhalima@gmail.com>
2023-03-21 06:52:22 -0700

    hello world proj

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void){
+	printf("Size of a char: %d bytes(s)",sizeof(char));
+	printf("Size of an int: %d bytes(s)",sizeof(int));
+	printf("Size of a long int: %d bytes(s)",sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)",sizeof(long long int));
+	printf("Size of a float: %d bytes(s)",sizeof(float));
+	return 0;
+}	
