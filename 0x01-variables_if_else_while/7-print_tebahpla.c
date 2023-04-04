5004e79490865f43bb3b1cf78c3091c6f2d25cfd
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-04 19:38:09 +0200

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lower case in reverse
+ *
+ * Return: returns always 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'z'; alp <= 'a'; alp--)
+		putchar(alp);
+
+	putchar('\n');
+
+	return (0);
+}
