f6b5e38ebb57baa6f424a914be387eaf21c5de31
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-04 18:57:22 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,28 @@ int main(void)
 	char alp;
 
 	for (alp = 'a'; alp <= 'z'; alp++)
-    if (alp != 'q' && alp != 'e')
-      putchar(alp);
+    		if (alp != 'q' && alp != 'e')
+      			#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lower case and upper case
+ *
+ * Return: returns always 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
+
+	putchar('\n');
+
+	return (0);
+}putchar(alp);
 
 	putchar('\n');
 
