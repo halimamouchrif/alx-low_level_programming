fcba93253744d8f7ebb4f97c3d378bebc883f2cf
halimamouchrif <mouchrifhalima@gmail.com>
2023-04-03 17:55:13 -0700

    task 1 to 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,5 +6,5 @@ int main(void)
 	printf("Size of a long int: %d bytes(s)",sizeof(long int));
 	printf("Size of a long long int: %d bytes(s)",sizeof(long long int));
 	printf("Size of a float: %d bytes(s)",sizeof(float));
-	return(0);
+	return (0);
 }	
