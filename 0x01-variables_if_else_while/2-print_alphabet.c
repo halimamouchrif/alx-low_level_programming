12947817475279ec75fc9b8d86e2cf8b75294f54
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-04 18:44:34 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    char alp;
+	char alp;
+	
+	for(alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
 
-    for (alp = 'a'; alp <= 'z'; alp++)
-        putchar(alp);
+	putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
