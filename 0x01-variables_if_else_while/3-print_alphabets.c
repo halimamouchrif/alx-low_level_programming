db398d5cef2e1467909efae5be70bad683aced0f
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-04 18:48:51 +0200

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lower case
+ *
+ * Return: returns always 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+  for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
+  
+	putchar('\n');
+
+	return (0);
+}
