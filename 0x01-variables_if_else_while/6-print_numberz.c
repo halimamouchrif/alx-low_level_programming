df0583ec6e3a00f8a62da7d5352c1c16c5673bb6
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-04 19:19:47 +0200

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - prints number from 0 to 9
+ *
+ * Return: returns always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+
+	printf("\n");
+
+	return (0);
+}
