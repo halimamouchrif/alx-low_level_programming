31986fac656fbcc56810344ac499d231c2a35053
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-04 19:08:37 +0200

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lower case except q and e
+ *
+ * Return: returns always 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		if (alp != 'q' && alp != 'e')
+		{
+			putchar(alp);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
