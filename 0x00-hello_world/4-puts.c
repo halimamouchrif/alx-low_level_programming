a1ff4db954325c8b3453f1be490b6c1cf9b7fca0
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-04 03:14:25 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * @brief This is the main function.
+ *
+ * @return int Returns 0 on successful execution.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
