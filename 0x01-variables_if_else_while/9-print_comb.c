6521603e07d02afd5bbe6b0784152d1dd903fb9b
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-04 19:59:09 +0200

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints number from 0 to 9
+ *
+ * Return: returns always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+    putchat(',');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
