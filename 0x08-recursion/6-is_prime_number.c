b7153c4a99a7d2b443a3aa574ba511de70b180bf
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-06 03:00:04 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,7 +7,7 @@
  */
 int prime_check(int x, int y)
 {
-	if (x % y == 0)
+	if (x % y == 0 || x < 2)
 		return (0);
 	if (x == y)
 		return (1);
@@ -20,7 +20,7 @@ int prime_check(int x, int y)
  */
 int is_prime_number(int n)
 {
-	if (n == 0 || n == 1)
+	if (n == 1)
 		return (1);
 	return (prime_check(n, 2));
 }
