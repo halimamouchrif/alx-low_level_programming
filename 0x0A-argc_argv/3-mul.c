f228ccd518065840be0fa781858835153d593745
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-11 02:56:23 +0200

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,20 +2,20 @@
 #include "main.h"
 
 /*
- * main - prints the number of arguments passed into it.
+ * main - prints the result of the multiplication, followed by a new line
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 for success
  */
 int main(int argc , char *argv[])
 {
-  int x;
-  if (argc != 3)
-  {
-    printf("Error");
-    return (1);
-  }
-  x = argv[1] * argv[2];
+	int x;
+	if (argc != 3)
+	{
+	printf("Error");
+	return (1);
+	}
+	x = (int) argv[1] * (int) argv[2];
 	printf("%d\n", x);
 
 	return (0);
