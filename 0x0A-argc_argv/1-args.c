6c092d01140e988c353b0e7eb1a3e81a8955ceab
Halima Mouchrif <mouchrifhalima@gmail.com>
2023-04-11 02:43:02 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: array of arguments
  * Return: 0 for success
  */
-int main(int argc , char *argv[])
+int main(int argc , char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc-1);
 
